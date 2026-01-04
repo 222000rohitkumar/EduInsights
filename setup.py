780d7843eeb111a952afde4923b00a024755e471
222000rohitkumar <rvempire007@gmail.com>
2026-01-04 18:30:11 +0530

    adding the whole project after completion

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 from setuptools import find_packages,setup
 from typing import List
-def get_requirements(file_path:str)->list[str]:
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
     requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace('\n','') for req in requirements]
-        if '-e .' in requirements:
-            requirements.remove('-e .')
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-    return requirements        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     
+    return requirements
 
-setup(    name="EduInsights",
-    version="0.0.1",
-    author="Wolverine",
-    author_email="rvempire007@gmail.com",
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+setup(
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
